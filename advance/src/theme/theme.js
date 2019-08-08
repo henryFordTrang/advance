1ad50bd1598579ecc856fe0435c59ad0d795aaf0
czd <763830568@qq.com>
2019-08-08 17:41:15 +0800

    :1

---
+++ advance/src/theme/theme.js
@@ -0,0 +1,24 @@
+const colors = {
+  black: '#212837',
+  white: '#fff'
+}
+
+module.exports = {
+  // nav
+  'nav-bar-icon-color'      : colors.black,
+  'nav-bar-text-color'      : colors.black,
+  'nav-bar-background-color': 'transparent',
+  'nav-bar-height'          : '44px',
+  'nav-bar-title-font-size' : '16px',
+  'nav-bar-arrow-size'      : '20px',
+
+  // toast
+  'toast-max-width': '270px',
+
+  // dialog
+  'dialog-width'            : '270px',
+  'dialog-border-radius'    : '6px',
+  'dialog-font-size'        : '16px',
+  'dialog-message-font-size': '14px'
+}
+
